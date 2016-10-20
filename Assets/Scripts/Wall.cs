74534bc18a8bf552afbb809a80bcfd4ffa727a05
jinhyeon <wlsgussla123@gmail.com>
2016-10-20 17:19:10 -0400

    Step 7. Creating destructible walls

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+    public Sprite dmgSprite; // can see attacked walls when player attack walls !
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+	
+    public void DamageWall (int loss)
+    {
+        spriteRenderer.sprite = dmgSprite; // assign attacked walls to the sprite.
+        hp -= loss;
+        if (hp <= 0)
+            gameObject.SetActive(false); // not equal enable. enable is for component
+    }
+}
